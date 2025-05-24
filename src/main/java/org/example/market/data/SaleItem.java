7e2905c4600b48a39e8d6276a8facf9b963e2815
v-ivanova03 <v.03.ivanova@abv.bg>
2025-05-24 10:07:42 +0300

    Add SaleItem class

---
+++ src/main/java/org/example/market/data/SaleItem.java
@@ -0,0 +1,55 @@
+package org.example.market.data;
+
+import java.io.Serializable;
+
+public class SaleItem implements Serializable {
+    private Product product;
+    private int quantity;
+    private double unitPrice;
+    private double totalPrice;
+
+    public SaleItem(Product product, int quantity, double unitPrice, double totalPrice) {
+        this.product = product;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void recalculateTotalPrice() {
+       this.totalPrice = unitPrice * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return product.getName() + " x" + quantity + " = " + totalPrice;
+    }
+
+}
