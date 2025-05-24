275abc01c0591be96b03b0f0fd7369da9fc570d5
v-ivanova03 <v.03.ivanova@abv.bg>
2025-05-24 11:08:14 +0300

    Update InsufficientQuantityException formatting

--- src/main/java/org/example/market/exception/InsufficientQuantityException.java
+++ src/main/java/org/example/market/exception/InsufficientQuantityException.java
@@ -1,7 +1,32 @@
 package org.example.market.exception;
 
-public class InsufficientQuantityException extends RuntimeException {
-  public InsufficientQuantityException(String message) {
-    super(message);
-  }
+public class InsufficientQuantityException extends Exception {
+    private final String productName;
+    private final int requestedQuantity;
+    private final int availableQuantity;
+
+    public InsufficientQuantityException(String productName, int requestedQuantity, int availableQuantity) {
+        super(("Insufficient quantity of: " + productName +
+                " | Requested: " + requestedQuantity + ", Available: " + availableQuantity));
+        this.productName = productName;
+        this.requestedQuantity = requestedQuantity;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getRequestedQuantity() {
+        return requestedQuantity;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return getMessage();
+    }
 }
