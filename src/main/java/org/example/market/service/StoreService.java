3e4e34c6cf350060809d5a8a96dc5e2e08c5c598
v-ivanova03 <v.03.ivanova@abv.bg>
2025-05-24 11:12:23 +0300

    Update StoreService

--- src/main/java/org/example/market/service/StoreService.java
+++ src/main/java/org/example/market/service/StoreService.java
@@ -1,4 +1,20 @@
 package org.example.market.service;
 
+import org.example.market.data.Cashier;
+import org.example.market.data.Product;
+import org.example.market.data.Receipt;
+import org.example.market.exception.InsufficientQuantityException;
+
+import java.util.List;
+import java.util.Map;
+
 public interface StoreService {
+    void addProduct(Product product);
+    void addCashier(Cashier cashier);
+    Receipt sell(Map<String, Integer> shoppingList, Cashier cashier) throws InsufficientQuantityException;
+    double getTotalRevenue();
+    double getTotalSalaryExpenses();
+    double getProfit();
+    int getReceiptCount();
+    List<Receipt> getReceipts();
 }
