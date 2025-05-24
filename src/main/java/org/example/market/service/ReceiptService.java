246d426ce210d238292711c64c091dcf70488001
v-ivanova03 <v.03.ivanova@abv.bg>
2025-05-24 12:40:54 +0300

    Update ReceiptService interface

--- src/main/java/org/example/market/service/ReceiptService.java
+++ src/main/java/org/example/market/service/ReceiptService.java
@@ -1,4 +1,10 @@
 package org.example.market.service;
 
+import org.example.market.data.Receipt;
+
 public interface ReceiptService {
+    void saveToFile(Receipt receipt);
+    String readFromFile(int number);
+    void serializeReceipt(Receipt receipt);
+    Receipt deserializeReceipt(int number);
 }
---
+++ src/main/java/org/example/market/service/impl/ReceiptServiceImpl.java
@@ -0,0 +1,4 @@
+package org.example.market.service.impl;
+
+public class ReceiptServiceImpl {
+}
