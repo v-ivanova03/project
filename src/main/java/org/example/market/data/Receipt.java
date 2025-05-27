881ea74e0e035f05d4323090eb1d8e5027ba1d55
v-ivanova03 <v.03.ivanova@abv.bg>
2025-05-27 18:08:03 +0300

    Update Receipt metod toString()

--- src/main/java/org/example/market/data/Receipt.java
+++ src/main/java/org/example/market/data/Receipt.java
@@ -2,7 +2,9 @@ package org.example.market.data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Receipt implements Serializable {
     private static int counter = 0;
@@ -42,14 +44,10 @@ public class Receipt implements Serializable {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Receipt #").append(number).append("\n");
-        sb.append("Cashier: ").append(cashier.getName()).append("\n");
-        sb.append("Date: ").append(dateTime).append("\n");
-        for (SaleItem item : items) {
-            sb.append(item).append("\n");
-        }
-        sb.append("Total: ").append(total);
-        return sb.toString();
+        return "Receipt #" + number + "\n" +
+                "Cashier: " + cashier.getName() + "\n" +
+                "Date: " + dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n" +
+                items.stream().map(SaleItem::toString).collect(Collectors.joining("\n")) + "\n" +
+                "Total: " + total;
     }
 }
